ea2067bb9f9d46b6693f1c68e9a65a22386ef8b7
Quin A <qelson@protonmail.com>
2023-03-28 04:58:54 -0400

    step 5

--- unit4miniProject.py
+++ unit4miniProject.py
@@ -46,4 +46,27 @@ print(f"Cost: ${total_cost:.2f}\n")
 
 total_sales = total_sales + total_cost
 
+carpet_price = float(input())
+room_width = int(input())
+room_length = int(input())
+
+room_area = room_width * room_length
+
+carpet_cost = carpet_price * room_area * 1.2
+
+labor_cost = room_area * 0.75
+
+sales_tax = (carpet_cost + labor_cost) * tax_rate
+
+total_cost = carpet_cost + labor_cost + sales_tax
+
+print("Order #3")
+print(f"Room: {room_area} sq ft")
+print(f"Carpet: ${carpet_cost:.2f}")
+print(f"Labor: ${labor_cost:.2f}")
+print(f"Tax: ${sales_tax:.2f}")
+print(f"Cost: ${total_cost:.2f}\n")
+
+total_sales = total_sales + total_cost
+
 print(f"Total Sales: ${total_sales: .2f}")
