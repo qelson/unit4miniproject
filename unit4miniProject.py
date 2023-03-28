092bbcf9c252a6b4a3e37b4e2da3d43b7466a9de
Quin A <qelson@protonmail.com>
2023-03-28 04:15:06 -0400

    step 2

--- unit4miniProject.py
+++ unit4miniProject.py
@@ -6,5 +6,8 @@ room_area = room_width * room_length
 
 carpet_cost = carpet_price * room_area * 1.2
 
+labor_cost = room_area * 0.75
+
 print(f"Room: {room_area} sq ft")
 print(f"Carpet: ${carpet_cost:.2f}")
+print(f"Labor cost: ${labor_cost:.2f}")
