5324972076582283f9410587fcd4b31ab28385e6
Quin A <qelson@protonmail.com>
2023-03-28 04:22:46 -0400

    step 3

--- unit4miniProject.py
+++ unit4miniProject.py
@@ -8,6 +8,13 @@ carpet_cost = carpet_price * room_area * 1.2
 
 labor_cost = room_area * 0.75
 
+tax_rate = 0.07
+sales_tax = (carpet_cost + labor_cost) * tax_rate
+
+total_cost = carpet_cost + labor_cost + sales_tax
+
 print(f"Room: {room_area} sq ft")
 print(f"Carpet: ${carpet_cost:.2f}")
-print(f"Labor cost: ${labor_cost:.2f}")
+print(f"Labor: ${labor_cost:.2f}")
+print(f"Tax: ${sales_tax:.2f}")
+print(f"Cost: ${total_cost:.2f}")
